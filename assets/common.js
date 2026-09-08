5106d49b7b3b5d97030d2f5a18b1605c47d08e58
chun-assistant <yonglinchun@gmail.com>
2026-09-09 02:21:59 +0800

    fix: restore shared runtime script loading

--- assets/common.js
+++ assets/common.js
@@ -19,23 +19,23 @@
   var isDailyPage = document.body && document.body.getAttribute("data-page") === "days";
 
   if (document.readyState === "loading" && document.write) {
-    document.write('<script src="' + packingDataSrc + '"><\\/script>');
-    document.write('<script src="' + coreDataSrc + '"><\\/script>');
-    document.write('<script src="' + printDataSrc + '"><\\/script>');
-    document.write('<script src="' + legacySrc.replace(/"/g, "&quot;") + '"><\\/script>');
-    document.write('<script src="' + base + 'modules/flight-badges.js' + '"><\\/script>');
+    document.write('<script src="' + packingDataSrc + '"><\/script>');
+    document.write('<script src="' + coreDataSrc + '"><\/script>');
+    document.write('<script src="' + printDataSrc + '"><\/script>');
+    document.write('<script src="' + legacySrc.replace(/"/g, "&quot;") + '"><\/script>');
+    document.write('<script src="' + base + 'modules/flight-badges.js' + '"><\/script>');
 
     if (isDailyPage) {
-      document.write('<script src="' + base + 'modules/daily-event-filters.js"><\\/script>');
-      document.write('<script src="' + base + 'modules/runtime-utils.js"><\\/script>');
-      document.write('<script src="' + base + 'data/apps.js"><\\/script>');
-      document.write('<script src="' + base + 'modules/apps-renderer.js"><\\/script>');
-      document.write('<script src="' + base + 'modules/apps.js"><\\/script>');
-      document.write('<script src="' + base + 'modules/apps-bootstrap.js"><\\/script>');
-      document.write('<script src="' + base + 'data/stay.js"><\\/script>');
-      document.write('<script src="' + base + 'modules/stay-renderer.js"><\\/script>');
-      document.write('<script src="' + base + 'modules/stay.js"><\\/script>');
-      document.write('<script src="' + base + 'modules/stay-bootstrap.js"><\\/script>');
+      document.write('<script src="' + base + 'modules/daily-event-filters.js"><\/script>');
+      document.write('<script src="' + base + 'modules/runtime-utils.js"><\/script>');
+      document.write('<script src="' + base + 'data/apps.js"><\/script>');
+      document.write('<script src="' + base + 'modules/apps-renderer.js"><\/script>');
+      document.write('<script src="' + base + 'modules/apps.js"><\/script>');
+      document.write('<script src="' + base + 'modules/apps-bootstrap.js"><\/script>');
+      document.write('<script src="' + base + 'data/stay.js"><\/script>');
+      document.write('<script src="' + base + 'modules/stay-renderer.js"><\/script>');
+      document.write('<script src="' + base + 'modules/stay.js"><\/script>');
+      document.write('<script src="' + base + 'modules/stay-bootstrap.js"><\/script>');
     }
     return;
   }
