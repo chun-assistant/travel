18e0e0fe86ae9f1594446baad1b259967e8f8dcc
chun-assistant <yonglinchun@gmail.com>
2026-09-11 00:20:57 +0800

    Start shared runtime after Daily data overrides

--- assets/common.js
+++ assets/common.js
@@ -19,6 +19,8 @@
   var legacySrc = base + "common-legacy.js";
   var isDailyPage = document.body && document.body.getAttribute("data-page") === "days";
 
+  if (isDailyPage) window.TRAVEL_DEFER_RUNTIME_INIT = true;
+
   if (document.readyState === "loading" && document.write) {
     document.write('<script src="' + packingDataSrc + '"><\/script>');
     document.write('<script src="' + coreDataSrc + '"><\/script>');
@@ -47,6 +49,7 @@
       document.write('<script src="' + base + 'modules/stay-renderer.js"><\/script>');
       document.write('<script src="' + base + 'modules/stay.js"><\/script>');
       document.write('<script src="' + base + 'modules/stay-bootstrap.js"><\/script>');
+      document.write('<script>window.TravelCommonRuntimeStart && window.TravelCommonRuntimeStart();<\/script>');
     }
     return;
   }
@@ -78,7 +81,10 @@
 
   var index = 0;
   function loadNext() {
-    if (index >= scripts.length) return;
+    if (index >= scripts.length) {
+      if (isDailyPage && window.TravelCommonRuntimeStart) window.TravelCommonRuntimeStart();
+      return;
+    }
     var script = document.createElement("script");
     script.src = scripts[index++];
     script.async = false;
